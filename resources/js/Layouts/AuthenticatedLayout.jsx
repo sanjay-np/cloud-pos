b4861367b2789b6c6c208c3bb585b03a004d7ee2
sanjay <sanjayc2051@gmail.com>
2024-09-02 14:02:10 +0545

    Initial commit

---
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import Sidebar from '@/Components/Theme/Sidebar';
+
+export default function Authenticated({ user, header, children }) {
+    return (
+        <React.Fragment>
+            <aside className='sidebar'>
+                <Sidebar />
+            </aside>
+            <div className="">
+                {/* <nav className="">
+                    <div className="">
+                        <Dropdown>
+                            <Dropdown.Trigger>
+                                <span className="inline-flex rounded-md">
+                                    <button
+                                        type="button"
+                                        className="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150"
+                                    >
+                                        {user.name}
+
+                                        <svg
+                                            className="ms-2 -me-0.5 h-4 w-4"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20"
+                                            fill="currentColor"
+                                        >
+                                            <path
+                                                fillRule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clipRule="evenodd"
+                                            />
+                                        </svg>
+                                    </button>
+                                </span>
+                            </Dropdown.Trigger>
+
+                            <Dropdown.Content>
+                                <Dropdown.Link href={route('profile.edit')}>Profile</Dropdown.Link>
+                                <Dropdown.Link href={route('logout')} method="post" as="button">
+                                    Log Out
+                                </Dropdown.Link>
+                            </Dropdown.Content>
+                        </Dropdown>
+                    </div>
+                </nav> */}
+
+                <main>{children}</main>
+            </div>
+        </React.Fragment>
+    );
+}
