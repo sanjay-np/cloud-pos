4b7a1a9e5a22ba5178461d5b98a9bccd2bb23b1e
Gouenji10 <excelblade10@gmail.com>
2024-09-16 08:37:49 +0545

    product categories

--- resources/js/Components/Categories/CategoryDrawer.jsx
+++ resources/js/Components/Categories/CategoryDrawer.jsx
@@ -1,12 +1,14 @@
 import { useForm } from "@inertiajs/react";
 import React, { useState } from "react";
-import { Button, Drawer } from "rsuite";
+import { Button, Drawer, Input, InputGroup, Uploader } from "rsuite";
+import InputError from "../InputError";
+import { AirplayIcon } from "lucide-react";
 
 export default function CategoryDrawer(props) {
 
 	const { open, setOpen, title, selected, setSelected } = props
 	const [loading, setLoading] = useState(false)
-
+	const [logo, setLogo] = useState(null)
 	const { data, setData, post, processing, errors, reset } = useForm({
 		name: '',
 		description: '',
@@ -30,6 +32,51 @@ export default function CategoryDrawer(props) {
 					</Drawer.Actions>
 				</Drawer.Header>
 				<Drawer.Body className="p-6">
+					<div className="mb-4">
+						<label className='text-gray-600 font-semibold mb-1 block'>Category Name</label>
+						<InputGroup>
+							<Input
+								placeholder="Category Name..."
+								value={data.name}
+								onChange={e => setData('name', e)}
+							/>
+						</InputGroup>
+						<InputError message={errors.name} className="mt-2" />
+					</div>
+					<div className="mb-4">
+						<label className='text-gray-600 font-semibold mb-1 block'>Description</label>
+						<InputGroup>
+							<Input
+								placeholder="Category Description..."
+								as="textarea"
+								rows={3}
+								value={data.description}
+								onChange={e => setData('description', e)}
+							/>
+						</InputGroup>
+					</div>
+					<div className="mb-4">
+						<label className='text-gray-600 font-semibold mb-1 block'>Category Logo</label>
+						<Uploader
+							className='category-logo-uploader'
+							fileListVisible={false}
+							listType="picture"
+							action="/"
+							autoUpload={false}
+							onChange={file => {
+								previewFile(file[0]?.blobFile, value => { setLogo(value); });
+								setData('image', file[0]);
+							}}
+						>
+							<button style={{ width: 140, height: 140 }}>
+								{logo ? (
+									<img src={logo} width="100%" height="100%" />
+								) : (
+									<AirplayIcon size={64} strokeWidth={1.2} color='gray' />
+								)}
+							</button>
+						</Uploader>
+					</div>
 				</Drawer.Body>
 			</Drawer>
 		</React.Fragment>
