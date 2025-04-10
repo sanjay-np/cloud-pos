771b85c31b244adf7be80a5cc3029c46e84cd2c6
sanjay-np <excelblade10@gmail.com>
2025-04-10 15:34:39 +0545

    chore: purchase and sales return

--- app/Http/Controllers/Purchase/PurchaseReturnController.php
+++ app/Http/Controllers/Purchase/PurchaseReturnController.php
@@ -5,4 +5,16 @@ namespace App\Http\Controllers\Purchase;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class PurchaseReturnController extends Controller {}
+class PurchaseReturnController extends Controller
+{
+    public function index(Request $request) {}
+
+    public function store() {}
+
+    public function show(int $id) {}
+
+    public function update(Request $request, int $id) {}
+
+
+    public function destroy(int $id) {}
+}
